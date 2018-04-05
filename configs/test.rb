82c27bc85b6eb04cc59e4968d95866494a9b4f31
Kasper Grubbe <kawsper@gmail.com>
2018-04-05 02:14:23 +0100

    Add configuration example

--- configs/test.rb
+++ configs/test.rb
@@ -1,1 +1,7 @@
-command "ruby examples/ruby_loop.rb"
+zvol 'Steam', '/dev/zvol/tank/steamdisk'
+zvol 'Creative', '/dev/zvol/tank/creativedisk'
+# raw 'Black and white 2', '/data/VMs/black_and_white2/qemu_vm.raw'
+
+set_setup   'sleep 1'
+set_command 'ruby examples/ruby_loop.rb'
+set_cleanup 'sleep 1'
